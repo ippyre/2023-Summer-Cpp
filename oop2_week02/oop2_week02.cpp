f3553dc351d132cc80a642faebab6c74ddb6d19a
ippyre <ippyre@naver.com>
2023-06-20 10:54:46 +0900

    get prime number by boolean type

--- oop2_week02/oop2_week02.cpp
+++ oop2_week02/oop2_week02.cpp
@@ -2,22 +2,26 @@
 using namespace std;
 
 int main() {
-	int number=0;
-	int count = 0;
+	int number = 0;
+	//int count = 0;
+	bool isPrime = true; //memory+, readability
+
 	cout << "Input number : ";
 	cin >> number;
 
-	for (int i = 1; i <= number; i++) {
+	for (int i = 2; i < number; i++) {
 		if (number % i == 0) {
-			count++;
+			//count++;
+			isPrime = false; //remove add operation
 		}
 	}
-	if (count == 2) {
+	if (isPrime==true) {
 		cout << number << " is prime number~\n";
 	}
 	else {
 		cout << number << " is NOT prime number~\n";
 	}
+
 
 	return 0;
 }
