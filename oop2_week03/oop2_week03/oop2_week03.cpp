9bf41d84073b96ab59a8edc9e3c6c3aeacb4efe8
ippyre <ippyre@naver.com>
2023-06-21 09:59:53 +0900

    week03 reference v0.4

--- oop2_week03/oop2_week03/oop2_week03.cpp
+++ oop2_week03/oop2_week03/oop2_week03.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void test(int &ra) {
+void test(int &ra) { // call by reference
 	ra = 7;
+	cout << &ra << '\n';
 }
 
 int main() {
 	int a = 1;
+	cout << &a << '\n';
 	cout << a << '\n';
 	test(a);
 	cout << a << '\n';
+	
 	return 0;
 }
