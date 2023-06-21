6ddae6defae720768ea8c155a7b0ba17cfb475ea
ippyre <ippyre@naver.com>
2023-06-21 11:17:22 +0900

    week03 string v0.1

--- oop2_week03/oop2_week03/oop2_week03.cpp
+++ oop2_week03/oop2_week03/oop2_week03.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 using namespace std;
 
-void test(int* pa) { // call by pointer
-	*pa = 7;
-	cout << pa << '\n';
-}
-
 int main() {
-	int a = 1;
-	cout << &a << '\n';
-	cout << a << '\n';
-	test(&a);
-	cout << a << '\n';
-	
+	int a = 9;
+	int* pa = &a;
+	int** ppa = &pa;
+	char univ[] = "inha\n";
+	//char univ[] = { 'i', 'n', 'h', 'a', '\n' };
+	//char univ[] = { 'i', 'n', 'h', 'a', '\n', '\0'};
+	//char univ[] = { 'i', 'n', '\0', 'h', 'a', '\n'};
+	//char univ[] = { 'i', 'n', 0, 'h', 'a', '\n' };
+	//char univ[] = { 'i', 'n', NULL, 'h', 'a', '\n' };
+	//char univ[] = { 'i', 'n', nullptr, 'h', 'a', '\n' };  // error
+
+	cout << univ;
+
+	ppa = nullptr;  // C++11
+
+	//ppa = 0;
+	//ppa = NULL;
+
+	//ppa = '\0';
+	if (ppa) {
+		cout << a << " " << *pa << " " << **ppa << '\n';
+		cout << &a << " " << pa << " " << *ppa << '\n';
+	}
+
 	return 0;
 }
+
+
