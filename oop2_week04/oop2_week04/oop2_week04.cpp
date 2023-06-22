de0bd40ebc036d89d8e60ae7296331fd378e6ff1
ippyre <ippyre@naver.com>
2023-06-22 10:37:01 +0900

    heap memory v0.1

--- oop2_week04/oop2_week04/oop2_week04.cpp
+++ oop2_week04/oop2_week04/oop2_week04.cpp
@@ -1,42 +1,36 @@
-//student class를 만들어서 필수 필드는 이름, 학번
-
 #include <iostream>
 #include <string>
 using namespace std;
 
-class Student {
-private:
-	int id;
-	string name;
-	static int count; // =0 에러 발생
-public:
-	Student() {//기본 생성자
-		cout << this << " default constructor!" << '\n';
-		count++;
-	}
-	~Student() {//소멸자
-		cout << this << " destructor activated..\n";
-		count--;
-	}
-	static int getCount() { //인라인 형태 -> 함수 호출에 오버헤드가 들지 않음
-		return count;
-	}
-};
-//생성자가 없기 때문에 기본 생성자가 생긴 상태 -> 객체를 생성할 때 count++해주고 싶은 상황
-int Student::count = 0;
 
-void test() {
-	Student s3;
-	cout << Student::getCount() << '\n';
-}
 
 int main() {
-	Student s1;
-	Student s2;
+	int noOfPeople = 0;
+	int total = 0;
 
-	cout << s1.getCount() << '\n'; //객체 인스턴
-	cout << Student::getCount() << '\n'; //클래스 메소드(자바에선 이렇게만 쓸 수 있음?);
-	test();
-	cout << Student::getCount() << '\n'; // 2
+	cout << "Input no of people : ";
+	cin >> noOfPeople;
+	int* pAge = new int[noOfPeople]; //dynamic memory allocation
+
+	for (auto i = 0; i < noOfPeople; i++) {
+		cout << "Input age : ";
+		cin >> *(pAge + i); // cin >> pAge[i];
+	}
+	for (auto i = 0; i < noOfPeople; i++) {
+		int age = pAge[i];
+		if (age >= 19)
+			total = total + 10000;
+		else if (age >= 8) {
+			total += 7000;
+		}
+		else {
+			total += 5000;
+		}
+		
+	}cout << "People : " << noOfPeople << ", Fee : " << total << '\n';
+	delete[] pAge; //free heap memory(prevent memory leak)
+	pAge = nullptr;
+
+
 	return 0;
 }
